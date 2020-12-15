d2dd757c3de1e42a12a9e1a60a893c01d96cf0e3
Jeremy Penning <4085868+pixeljava@users.noreply.github.com>
2020-12-15 15:15:54 -0800

    Finished with tests!

--- javascript/code-challenges/linked-lists/linked-list/__tests__/linked-list.test.js
+++ javascript/code-challenges/linked-lists/linked-list/__tests__/linked-list.test.js
@@ -5,13 +5,13 @@ const LinkedList = require('../linked-list');
 
 // Write tests to prove the following functionality:
 
-//     Can successfully instantiate an empty linked list
-//     Can properly insert into the linked list
-//     The head property will properly point to the first node in the linked list
-//     Can properly insert multiple nodes into the linked list
-//     Will return true when finding a value within the linked list that exists
-//     Will return false when searching for a value in the linked list that does not exist
-//     Can properly return a collection of all the values that exist in the linked list
+//  X  Can successfully instantiate an empty linked list
+//  X  Can properly insert into the linked list
+//  X  The head property will properly point to the first node in the linked list
+//  X  Can properly insert multiple nodes into the linked list
+//  X  Will return true when finding a value within the linked list that exists
+//  X  Will return false when searching for a value in the linked list that does not exist
+//  X  Can properly return a collection of all the values that exist in the linked list
 
 describe('Linked List', () => {
   it('inserts a new node at the list beginning', () => {
@@ -26,14 +26,21 @@ describe('Linked List', () => {
     list.insert('thing');
     list.insert('other thing');
     list.insert('more stuff');
-    expect(list.includes('thing').toBe(true));
+    expect(list.includes('thing')).toBe(true);
+  });
+  it('includes - returns false if value is not in list', () => {
+    const list = new LinkedList();
+    list.insert('thing');
+    list.insert('other thing');
+    list.insert('more stuff');
+    expect(list.includes('exists')).toBe(false);
   });
   it('turns the values into a string', () => {
     const list = new LinkedList();
     list.insert('thing');
     list.insert('other thing');
     list.insert('more stuff');
-    expect(list.toString().toEqual('{more stuff} -> {other thing} -> {thing} -> {NULL}'));
+    expect(list.toString()).toEqual('{more stuff} -> {other thing} -> {thing} -> {NULL}');
   });
   // expected string output: {head, value:'thing', next: {{head, value:'other thing', next: {{head, value:'more stuff', next: {}}}}
 });
